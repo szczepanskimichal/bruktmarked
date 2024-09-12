82e1508e1963b1d0c3f9cf8a259bc1fc332e98ba
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-12 16:41:40 +0200

    Working over background displaying
    -add pictures
    - Add motion animation to images in index.js -> Home component using framer-motion and integrate layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
-      <div>HOME</div>
+      <div className="w-full h-full flex justify-center items-center">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.5, 1)}
+          initial="hidden"
+          whileInView="show"
+          className="fixed bottom-0 left-[200px] w-[400px]"
+        >
+          <img src="1.png" alt="nike" />
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", "spring", 0.7, 1)}
+          initial="hidden"
+          whileInView="show"
+          className="fixed top-[50px] right-[200px]"
+        >
+          <img src="2.png" alt="nike" className="w-[400px]" />
+        </motion.div>
+      </div>
     </Layout>
   );
 }
