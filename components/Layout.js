ea48455e18b04ec557b4657d8e0b7ef4a5119776
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-12 14:32:34 +0200

    Background for mainPage

--- components/Layout.js
+++ components/Layout.js
@@ -4,8 +4,11 @@ export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-[100vh]">
       <Header />
-      <div className="min-h-screen pt-[80px] flex justify-center">
-        <div className="w-full p-5">{children}</div>
+      <div className="min-h-screen pt-[60px] flex justify-center">
+        <div className="bg-color-600 fixed top-[150px] left-[120px] w-[200px] sm:w-[300px] h-[200px] rounded-full blur-[80px]" />
+        <div className="bg-color-600 fixed bottom-[120px] right-[100px] w-[200px] sm:w-[350px] h-[200px] rounded-full blur-[80px]" />
+        <div className="bg-primary fixed top-1/2 left-1/2 -translate-y-/1/2 -translate-x-1/2 size-[300px] sm:size-[500px] blur-[150px] rounded-full" />
+        <div className="w-full p-5 backdrop-blur-md">{children}</div>
       </div>
     </div>
   );
