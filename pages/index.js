ddf3c9ae655266bec8c1e77261d343bd3757c5e5
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-24 21:51:30 +0200

    UPdate Index.js - Refactor Home component to include user session and update UserInfoDiv
    - Added `useSession` hook from NextAuth to handle user session data.
    - Updated `UserInfoDiv` to `UserIndexDiv` and passed user data from session.
    - Kept existing layout and animations with Framer Motion for a smooth experience.

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import { Inter } from "next/font/google";
 import Layout from "@/components/layout/Layout";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/utils/motion";
-import UserInfoDiv from "@/components/layout/UserInfoDiv";
+import UserInfoDiv from "@/components/layout/UserIndexDiv";
+import { useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const session = useSession();
+
   return (
     <Layout>
       <div className="w-full h-full flex justify-center items-center">
@@ -26,7 +29,7 @@ export default function Home() {
         >
           <img src="2.png" alt="nike" className="w-[400px] scale-x-[-1]" />
         </motion.div>
-        <UserInfoDiv />
+        <UserInfoDiv user={session?.data?.user} />
       </div>
     </Layout>
   );
