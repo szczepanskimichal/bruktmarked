f43a9c15231a3551c15bad50be695462c69c2860
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-25 22:42:46 +0200

    Refactor UserIndexDiv component to include user email in welcome message, now everyth, now everything happens dynamically. After logging in, the user's email is displayed immediately

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const session = useSession();
-
+  // console.log(session);
   return (
     <Layout>
       <div className="w-full h-full flex justify-center items-center">
