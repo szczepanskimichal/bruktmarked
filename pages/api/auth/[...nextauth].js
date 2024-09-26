417d782756334c4d91c269f35298ee5b0812de29
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-26 21:27:36 +0200

    In Product Cart we have a heart name to like the price and a candy for cards with svg

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -36,13 +36,29 @@ export const authOptions = {
         await mongooseConnect();
         const user = await User.findOne({ email });
         if (user && bcrypt.compareSync(password, user.password)) {
-          return user;
+          return {
+            id: user._id.toString(),
+            email: user.email,
+            name: user.name,
+          };
         } else {
           return null;
         }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
