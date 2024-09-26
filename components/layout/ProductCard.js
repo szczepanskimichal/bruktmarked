b654116a42991e791ffd8702dadee6eb76cd3e57
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-26 20:57:38 +0200

    Picture + name + some css

--- components/layout/ProductCard.js
+++ components/layout/ProductCard.js
@@ -9,18 +9,27 @@ export default function ProductCard({
   index,
 }) {
   return (
-    <div>
-      <div>
+    <div className="box">
+      <div className="bg-white h-[200px] p-3 mb-2 rounded-t-lg flex justify-center items-center">
         <Link href={`/products/${_id}`}>
           {images?.length > 0 ? (
             <img src={images[0]} className="max-h-[180px] rounded-lg"></img>
           ) : (
             <img
               src="https://vilo.krakow.pl/wp-content/uploads/2020/07/emptyimagee.jpg"
-              className="rounded-lg p-1"
+              className="rounded-lg max-h-[180px] p-1"
             ></img>
           )}
         </Link>
+      </div>
+      <div className="p-3">
+        <div className="flex justify-between items-center">
+          <Link href={`/products/${_id}`}>
+            <h3 className="text-lg hover:text-primary hover:decoration-primary decoration-gray-100 underline transition-all delay-150 duration-300">
+              {title}
+            </h3>
+          </Link>
+        </div>
       </div>
     </div>
   );
